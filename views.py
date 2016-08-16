8c510513bc089dabfb6c0deacc5b2b3431be0849
rajeshkumarramaswamy <rajeshkramaswamy@gmail.com>
2016-08-16 08:55:43 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 
 
 def index(request):
-	data = {
-	"Azure NOC" : ["2nd Watch", "2W Demo"],
-	"ANN NOC" : ["Accudata System", "ACES"],
-	"BVRM NOC" : ["ACUTEC", "Adelbrook"],
-	"CBU AID1" : ["AKUITY Technologies", "Aqueduct Technologies", "Axis Business Solutions", "Databranch Inc", "Datavox1"],
-	" CBU Manage1" : ["", ""],
-}
-	return render(request, 'index.html', {'data': data})
+    data = {
+        "Azure NOC": ["2nd Watch", "2W Demo"],
+        "ANN NOC": ["Accudata System", "ACES"],
+        "BVRM NOC": ["ACUTEC", "Adelbrook"],
+        "CBU AID1": ["AKUITY Technologies", "Aqueduct Technologies", "Axis Business Solutions", "Databranch Inc",
+                     "Datavox1"],
+        " CBU Manage1": ["All Information Services", "ATech Support"],
+    }
+    return render(request, 'index.html', {'data': data})
