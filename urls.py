2f22acecd012ea452ce57ea4f1a880eb449fb08f
rajeshkumarramaswamy <rajeshkramaswamy@gmail.com>
2016-08-17 07:48:18 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,7 +1,25 @@
-from django.conf.urls import url, include, patterns
+"""nnc URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from dashboard.views import *
+from django.views.generic import TemplateView
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    #url(r'^dashboard/', 'views.index', name='home'),
-)
-
+urlpatterns = [
+    url(r'^$', 'dashboard.views.index', name='home'),
+    url(r'^dashboard/$', TemplateView.as_view(template_name='filters.html'), name='filters'),
+    url(r'^admin/', include(admin.site.urls)) #pass@123(rajesh)
+]
